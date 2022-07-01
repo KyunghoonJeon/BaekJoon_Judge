fd468c5e0edde902df7201bc7e1e567ea20f284d
KyunghoonJeon <106662504+KyunghoonJeon@users.noreply.github.com>
2022-07-01 18:15:27 +0900

    Add files via upload

---
+++ Stack/question9012.py
@@ -0,0 +1,16 @@
+N=int(input())
+for i in range(N):
+    l=list(input())
+    sum=0
+    for i in l:
+        if i=='(':
+            sum+=1
+        elif i==')':
+            sum-=1
+        if sum<0:
+            print('NO')
+            break
+    if sum>0:
+        print('NO')
+    elif sum==0:
+        print('YES')
