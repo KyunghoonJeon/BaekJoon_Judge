acba33f138fe20c18eba8f0ff53d0a269efbd434
KyunghoonJeon <106662504+KyunghoonJeon@users.noreply.github.com>
2022-07-13 15:15:03 +0900

    Add files via upload

---
+++ Number_Theory/question11051.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+n,k=map(int, input().split())
+mul1=1
+for i in range(n,n-k,-1):
+    mul1*=i
+mul2=1
+for j in range(1,k+1):
+    mul2*=j
+val=int(mul1//mul2)
+print(val%10007)
