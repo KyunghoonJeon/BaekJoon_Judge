32093b1dda41305606e5388adf32c3c794d966b1
KyunghoonJeon <106662504+KyunghoonJeon@users.noreply.github.com>
2022-08-03 13:49:01 +0900

    Add files via upload

---
+++ Divide_n_Conquer/question2740.py
@@ -0,0 +1,21 @@
+import sys
+input=sys.stdin.readline
+n,m=map(int,input().split())
+A=[]
+for _ in range(n):
+    A.append(list(map(int,input().split())))
+m,k=map(int,input().split())
+B=[]
+for _ in range(m):
+    B.append(list(map(int,input().split())))
+C=[[0 for _ in range(k)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(k):
+        for l in range(m):
+            C[i][j]+=A[i][l]*B[l][j]
+print(C)
+for i in C:
+    for j in i:
+        print(j,end=' ')
+    print()
